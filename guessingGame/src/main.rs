146bce4b6c49683234d890786d2feaf14fb0a433
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2023-01-25 15:55:19 +0100

    feat: update

--- guessingGame/src/main.rs
+++ guessingGame/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main(){
+    println!("Guesse the number");
+
+    let secret_number = rand::tread_rng().gen_range(1..=100);
+    
+    loop{
+        println!("Please input your guess");
+    }
 }
